0b7e45f14767c6b27345b937a5887b8439853154
krasterisk <info@krasterisk.ru>
2025-11-17 10:08:47 +0700

    update audio session delete function

--- src/open-ai/open-ai.connection.ts
+++ src/open-ai/open-ai.connection.ts
@@ -47,6 +47,7 @@ export class OpenAiConnection {
 
     private handleMessage(data) {
         const event = JSON.parse(data.toString());
+        console.log("EVENT: ",event)
         // Передаем события с привязкой к channelId
         this.eventEmitter.emit(`openai.${this.channelId}`, event);
     }

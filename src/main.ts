0aa825109d3d1f8ffd4a43dfaa4fc19cccc7cf97
krasterisk <info@krasterisk.ru>
2026-02-01 16:58:50 +0700

    Change notifications

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function start() {
     console.log(`${process.env.NODE_ENV}`)
     const PORT = process.env.PORT
     const app = await NestFactory.create(AppModule, { rawBody: true })
-    app.setGlobalPrefix('api')
+    app.setGlobalPrefix('api', { exclude: ['static/(.*)'] })
 
 
     const config = new DocumentBuilder()
